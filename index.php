05d65ad91b5d5ec82bfa4b157e8d8f4eda6df89c
bmk2010 <profiy70@gmail.com>
2026-06-02 23:15:36 +0500

    test php code

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Get data html</h1>
+    <form action="POST">
+        <label for="username">Enter username:1</label>
+        <input type="text" name="username" id="username">
+        <button type="submit" name="submit">Submit</button>
+    </form>
+
+    <hr>
+
+    
+</body>
+</html>
